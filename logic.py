a05842b9a45897bf23df465dbc74185399f2c13c
CarsonCoolMan <carsonjholmes@gmail.com>
2025-05-01 14:30:06 -0500

    Fixed

--- logic.py
+++ logic.py
@@ -4,11 +4,19 @@ import csv
 import sys
 import os
 
-"""
-put ID as said in minimum video
-"""
 class VotingApp(QDialog):
+    """
+    APP that allows users to enter a voter ID and
+    select a candidate using radio buttons and click
+    the vote button to vote
+    """
     def __init__(self):
+        """
+        Initializes the VotingApp Dialog
+        sets up the UI and connects
+        the vote button the the handle_vote
+        function
+        """
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
@@ -17,24 +25,28 @@ class VotingApp(QDialog):
 
     def handle_vote(self):
         """
-        self.radiobutton_is_selected() checks if a radio button is selected
-        self.check_id() checks if the ID is a duplicate or not a number, and if it
-        is it will return True causing the code not to continue
+        Handle the voting submission process
+
+        - checks if a candidate is selected
+        - Checks if there is duplicates or letters, if so the code will not
+        continue
+        - Records the vote if its valid
+        - upadtes labels
         """
         if (self.radiobutton_is_selected()):
-            if(self.check_id() == False):
+            if(self.check_id()):
                 self.record_vote()
                 self.ui.label_already_voted.setText("Voted!!")
                 self.clear_app()
 
     def radiobutton_is_selected(self):
         """
-        If either radio button is selected
-        :return: True
+        checks if a radio button is selected
 
-        If both radio buttons are not selected then
-        already voted label changes to Must select one
-        :return: False
+        Returns:
+            :bool: True if one is selected False otherwise
+            Updates already voted label if there is none
+            selected
         """
         if self.ui.radiobutton_carson.isChecked() or self.ui.radiobutton_john.isChecked():
             return True
@@ -44,10 +56,7 @@ class VotingApp(QDialog):
 
     def clear_app(self):
         """
-        clears the textedit
-
-        :arg: rb in [a,b]
-        unselected both radio buttons
+        clears the textedit and both radio buttons
         """
         self.ui.textedit_id.clear()
 
@@ -57,6 +66,10 @@ class VotingApp(QDialog):
             rb.setAutoExclusive(True)
 
     def record_vote(self):
+        """
+        Records the user's vote to "voting_logs.csv:
+        :return:
+        """
         voter_id = self.ui.textedit_id.toPlainText()
 
         if self.ui.radiobutton_carson.isChecked():
@@ -76,10 +89,14 @@ class VotingApp(QDialog):
         voter_id = self.ui.textedit_id.toPlainText()
         filename = 'id_log.csv'
         """
-        :param: voter_id if that is a digit then it creates either a new file if there 
-        isnt one or it reads a file and decides if it is a dupliccate or not
-        the else just returns True because it is a letter 
-        :return: returns True if there is a duplicate or if the input is not a decimal
+        Checks if the voter ID is valid
+        
+        Checks if the ID is a number and not a duplicate reading
+        from the CSV file "id_log.csv" 
+        If the file does not exist it is created
+        
+        :return:
+            :bool: True if ID is valid and not duplicated or is a number, False otherwise
         """
 
         if voter_id.isdigit():
@@ -91,21 +108,21 @@ class VotingApp(QDialog):
                         if int(voter_id) == int(row[0]):
                             self.ui.label_already_voted.setText("Already Voted")
                             self.ui.label.setText(" ")
-                            return True
+                            return False
 
                 with open(filename, 'a', newline='') as file:
                     writer = csv.writer(file)
                     writer.writerow([int(voter_id)])
-                    return False
+                    return True
 
             except FileNotFoundError:
                 with open(filename, 'a', newline='') as file:
                     writer = csv.writer(file)
                     writer.writerow([int(voter_id)])
-                    return False
+                    return True
         else:
             self.ui.label.setText("Enter a \n number")
-            return True
+            return False
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QDialog
-import sys
 from logic import *
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
